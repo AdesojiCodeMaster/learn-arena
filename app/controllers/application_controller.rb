9faa6c22501cf80a80835816dd79769ec4991d75
AdesojiCodeMaster <adesoji.adewumi@yahoo.com>
2021-11-02 02:49:59 +0100

    Install devise, redesign signup and login pages, test the pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:firstname, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:firstname, :email, :password, :current_password)}
+  end
+
 end
